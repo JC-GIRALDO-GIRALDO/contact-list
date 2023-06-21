91537ce1e4a0207a60f0cf3e18c22e1fb0ab61c8
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-06-21 15:36:12 -0500

    lista de contactos solo con nombre y apellido

--- Js/app.js
+++ Js/app.js
@@ -1,26 +1,30 @@
-// funcion para generar boton de borrar
+// Array para almacenar las listas generadas
+const listasGeneradas = [];
+
+// funcion para generar el boton de borrar
 function crearBotonBorrar() {
-  const botonBorrar = document.createElement("button");
-  botonBorrar.textContent = "Borrar";
-  botonBorrar.classList.add("boton-borrar");
+  
+  const botonBorrar = document.createElement("button"); // Se crea un boton
+  botonBorrar.textContent = "Borrar"; //nombre del boton
+  botonBorrar.classList.add("boton-borrar"); //clase del boton
   botonBorrar.addEventListener("click", () => {
-    botonBorrar.parentElement.remove();
+    botonBorrar.parentElement.remove(); //funcion flecha para generar la accion de borrar del boton creado
   });
   return botonBorrar;
 }
 
 // funcion para generar boton de editar el texto
 function editarParrafo(nuevoParrafo) {
-  const botonParrafo = document.createElement("button");
-  botonParrafo.textContent = "Editar";
-  botonParrafo.classList.add("editar-parrafo");
+
+  const botonParrafo = document.createElement("button"); // Se crea un boton
+  botonParrafo.textContent = "Editar"; //nombre del boton
+  botonParrafo.classList.add("editar-parrafo"); //clase del boton
   botonParrafo.addEventListener("click", () => {
-    const editarInput = document.createElement("input");
+    const editarInput = document.createElement("input"); 
     editarInput.value = nuevoParrafo.textContent;
     nuevoParrafo.textContent = "";
     nuevoParrafo.appendChild(editarInput);
-    editarInput.focus();
-
+    
     editarInput.addEventListener("blur", () => {
       nuevoParrafo.textContent = editarInput.value;
     });
@@ -28,11 +32,24 @@ function editarParrafo(nuevoParrafo) {
   return botonParrafo;
 }
 
+// funcion principal con la que se crea la lista 
 let copiarLista = () => {
-  //me trae el nombre del input
+  //me trae el valor del nombre desde html input nombre
   const nombre = document.querySelector(".nombre").value;
-  //me trae el apellido del input
+  //condicional para que se deba ingresar el nombre
+  if(nombre.length === 0){
+    alert("El 'nombre' no ha sido ingresado");
+    return nombre;
+  }
+  
+  //me trae el valor del apellido desde html input apellido
   const apellido = document.querySelector(".apellido").value;
+  //condicional para que se deba ingresar el apellido
+  if(apellido.length === 0){
+    alert("El 'apellido' no ha sido ingresado");
+    return apellido;
+  }
+
   //se creo un array con nombre y apellido
   const arregloNombre = [nombre, apellido];
   //junte nombre y apellido juntos
@@ -56,84 +73,24 @@ let copiarLista = () => {
 
   elemento.appendChild(nuevaLista);
 
+  // Agregar la lista generada al array
+  listasGeneradas.push(nombreCompleto);
+
   //vuelve a poner los espacios del input en blanco
   document.querySelector(".nombre").value = "";
   document.querySelector(".apellido").value = "";
 };
 
-/*
-function crearBotonBorrar() {
-  const botonBorrar = document.createElement("button");
-  botonBorrar.textContent = "Borrar";
-  botonBorrar.classList.add("boton-borrar");
-  botonBorrar.addEventListener("click", () => {
-    botonBorrar.parentElement.remove();
+// Función para imprimir todas las listas generadas en la consola
+let imprimirLista = () => {
+  //condicional por si no se ha generado listas nuevas
+  if (listasGeneradas.length === 0) {
+    alert("No hay listas para imprimir");
+    return;
+  }
+
+  listasGeneradas.forEach((lista) => {
+    alert(`${lista}`);
   });
-  return botonBorrar;
-}
-
-let copiarLista = () => {
-  const nombre = document.querySelector(".nombre").value;
-  const apellido = document.querySelector(".apellido").value;
-  const arregloNombre = [nombre, apellido];
-  const nombreCompleto = arregloNombre.join(" ");
-  console.log(nombreCompleto);
-
-  const elemento = document.getElementById("agregar-lista");
-  const nuevaLista = document.createElement("div");
-
-  const nuevoParrafo = document.createElement("p");
-  nuevoParrafo.textContent = nombreCompleto;
-  nuevaLista.appendChild(nuevoParrafo);
-
-  const botonBorrar = crearBotonBorrar(); // Crear el botón de borrar
-  nuevaLista.appendChild(botonBorrar); // Agregar el botón junto con la lista
-
-  elemento.appendChild(nuevaLista);
-
-  document.querySelector(".nombre").value = "";
-  document.querySelector(".apellido").value = "";
-
-  
 };
-
-*/
-
-/*let copiarLista = () => {
-    const nombre = document.querySelector(".nombre").value;
-    const apellido = document.querySelector(".apellido").value;
-    const arregloNombre = [nombre, apellido];
-    const nombreCompleto = arregloNombre.join(" ");
-    console.log(nombreCompleto);
   
-    const elemento = document.getElementById("agregar-lista");
-    elemento.innerHTML += nombreCompleto;
-  
-  };
-*/
-/*
-let copiarLista = () => {
-    const nombre = document.querySelector(".nombre").value;
-    const apellido = document.querySelector(".apellido").value;
-    const arregloNombre = [nombre, apellido];
-    const nombreCompleto = arregloNombre.join(" ");
-    const url = "../HTML/pagina2.html?nombre=" + nombreCompleto;
-    window.location.href = url;
-  };
-  
-*/
-/*
-let copiarLista = () => {
-  // Obtener los parámetros de la URL
-const params = new URLSearchParams(window.location.search);
-const nombre = params.get('.nombre');
-const apellido = params.get('.apellido');
-const arregloNombre = [nombre, apellido];
-const nombreCompleto = arregloNombre.join(" ");
-
-// Mostrar los datos en la página
-const elemento = document.getElementById("agregar-lista");
-elemento.textContent = nombreCompleto;
-}
-
-*/
