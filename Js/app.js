3e59bd4204d4a7c393028515258375e3f36e71e2
JC-GIRALDO-GIRALDO <kmilom19@gmail.com>
2023-06-27 22:34:37 -0500

    trabajo listo para entregar

--- Js/app.js
+++ Js/app.js
@@ -1,19 +1,13 @@
 //-------------------------TRABAJO CONTACT-LIST--------------------------//
 
 const listaDeContactos = [];
-/* Cree una función flecha para generar los datos pedidos por el trabajo 
-   "CONTACT-LIST" en el cual pide que se ingrese (id, nombre completo, 
-   teléfono, ciudad, Dirección). Dentro de la función creo cuatro constantes,
-   una con los datos de ubicación, otra con la constante de "nombreCompleto", 
-   otra donde junto al nombre y apellido y la otra con los datos en general 
-   que va a tener la lista. */
-const agregarContacto = (id, nombre, apellido, telefono, ciudad, direccion) => {
 
+const agregarContacto = (id, nombre, apellido, telefono, ciudad, direccion) => {
   const datoDelNombre = {
     nombre: nombre, // Objeto con datos del nombre.
     apellido: apellido,
   };
-  const nombreCompleto = Object.values(datoDelNombre).join(" "); // Junto nombre y apellido.
+  const nombreCompleto = Object.values(datoDelNombre).join(" "); // contiene los valores de la propiedade (datoDelNombre)
 
   const datoUbicacion = {
     ciudad: ciudad, // Objeto con datos de ubicación.
@@ -21,24 +15,20 @@ const agregarContacto = (id, nombre, apellido, telefono, ciudad, direccion) => {
   };
 
   const datosLista = {
-    id: id, // Recibe id
-    nombre: nombreCompleto, // Recibe nombre completo
-    telefono: telefono, // Recibe telefono
-    ubicacion: datoUbicacion, // Recibe ciudad y direccion
+    id: id,
+    nombre: nombreCompleto,
+    telefono: telefono,
+    ubicacion: datoUbicacion,
   };
   listaDeContactos.push(datosLista); // Para enviar la lista a la lista de contactos y no tener que estar copiando y pegando parámetros.
 };
 
-// Función que me permite borra una lista de contacto según el índice.
-const eliminarContacto = (id) => { // Defino una constante con el parámetro id con el cual relaciono la lista. 
-  const indice = listaDeContactos.findIndex((contacto) => contacto.id === id);// Genero una función callback que recorre cada elemento de la lista y me trae la id.
+const eliminarContacto = (id) => {
+  // Defino una constante con el parámetro id con el cual relaciono la lista.
+  const indice = listaDeContactos.findIndex((contacto) => contacto.id === id); //findIndex, para encontrar el índice de un elemento
 
-  if (indice !== -1) { /*findIndex() devuelve -1 cuando no se encuentra ningún elemento que cumpla con la condición especificada. En este caso, si se encuentra 
-                         un contacto con el ID especificado, indice será un valor distinto de -1 y el código dentro de este bloque if se ejecutará.*/
-
-    listaDeContactos.splice(indice, 1); /* Si se cumple la condición anterior, se utiliza el método splice() en el array listaDeContactos para eliminar un 
-                                           elemento en la posición indice. El número 1 como segundo argumento indica que se eliminará un solo elemento a 
-                                           partir de esa posición.*/
+  if (indice !== -1) {
+    listaDeContactos.splice(indice, 1);
     console.log(`Contacto con ID ${id} eliminado correctamente.`);
   } else {
     console.log(`No se encontró ningún contacto con ID ${id}.`);
@@ -47,14 +37,11 @@ const eliminarContacto = (id) => { // Defino una constante con el parámetro id 
 
 // Función que me permite editar una lista de contacto según el índice.
 const actualizarContacto = (id, nuevosDatos) => {
-  const indice = listaDeContactos.findIndex((contacto) => contacto.id === id);// nuevosDatos es un objeto que contiene los nuevos datos que se utilizarán para actualizar el contacto.
-  if (indice !== -1) {  
+  const indice = listaDeContactos.findIndex((contacto) => contacto.id === id); // nuevosDatos es un objeto que contiene los nuevos datos que se utilizarán para actualizar el contacto.
+  if (indice !== -1) {
     const contactoActualizado = {
-      ...listaDeContactos[indice],  /*Los tres puntos (...) en JavaScript se conocen como el operador de propagación (spread operator), 
-                                      Permite descomponer un array en sus elementos individuales, Permite combinar múltiples arrays en uno solo,  
-                                      Permite realizar copias superficiales de arrays y objetos en lugar de referencias, Permite pasar múltiples 
-                                      argumentos a una función como un array */
-      ...nuevosDatos
+      ...listaDeContactos[indice], // (spread operator)
+      ...nuevosDatos,
     };
     listaDeContactos[indice] = contactoActualizado;
     console.log(`Contacto con ID ${id} actualizado correctamente.`);
@@ -64,9 +51,11 @@ const actualizarContacto = (id, nuevosDatos) => {
 };
 
 // Imprimir en Pantalla por medio del alert.
-const imprimirListaContactos = () => { // A la función le paso "listaDeContactos"
-  let mensaje = "Lista de contactos:\n\n"; // Un pequeño mensaje y seguido todas las listas debajo
-  listaDeContactos.forEach((contacto) => {  // Itero sobre los elementos de un array y ejecuto el mensaje que va a llevar cada ítem.
+const imprimirListaContactos = () => {
+  // A la función le paso "listaDeContactos"
+  let mensaje = "Lista de contactos:\n\n";
+  listaDeContactos.forEach((contacto) => {
+    //iterar sobre los elementos
     mensaje += `ID: ${contacto.id}\n`;
     mensaje += `Nombre: ${contacto.nombre}\n`;
     mensaje += `Teléfono: ${contacto.telefono}\n`;
@@ -74,7 +63,7 @@ const imprimirListaContactos = () => { // A la función le paso "listaDeContacto
     mensaje += `Dirección: ${contacto.ubicacion.direccion}\n\n`;
   });
   alert(mensaje);
-};
+}; // bloquear alert para que no se detenga en programa para ver la edicion de la lista.
 
 /* Generar listas llamando a la función "agregarContacto()"
           Id: escribe el id
@@ -129,6 +118,7 @@ agregarContacto(
 console.log(listaDeContactos);
 // Elimino una lista con él, id.
 eliminarContacto(1894981257);
+eliminarContacto(89425458);
 // Muestro todas las listas generadas ya sin la eliminada
 console.log(listaDeContactos);
 // Imprimo por consola las listas restantes
@@ -137,8 +127,8 @@ imprimirListaContactos(listaDeContactos);
 actualizarContacto(1058981258, {
   nombre: "Edison",
   telefono: 3201234567,
-  direccion: "calle 55 # 32e-28"
+  direccion: "calle 55 # 32e-28",
 });
 console.log(listaDeContactos);
 // Muestro todas las listas generadas sin la eliminada y con la editada
-imprimirListaContactos(listaDeContactos);
+console.log(listaDeContactos);
